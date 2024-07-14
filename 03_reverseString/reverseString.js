74a972b0ceba0fadf9e032e20c70e75f75d4334d
SimpleNX <simplepanigrahi17@gmail.com>
2024-07-14 22:16:35 +0530

    Add function for reversing string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(initialString) {
+    const chars = [];
+    let finalString = "";
+    for(const char of initialString)
+        chars.push(char);
+    let n = chars.length;
+    for(let i = 1; i<=n; i++)
+        finalString += chars.at(-i);
+    return finalString;
 };
 
 // Do not edit below this line
