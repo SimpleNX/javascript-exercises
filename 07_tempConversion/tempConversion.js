b429b0b8a0800dc3d3415c22ccf3189de25db2a8
SimpleNX <simplepanigrahi17@gmail.com>
2024-07-16 18:43:21 +0530

    Edit tempConvestion.js to add function for conversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
-};
+const convertToCelsius = function(tempFahrenheit) {
+    let tempCelsius = (tempFahrenheit-32)*5;
+	tempCelsius /= 9;
+	tempCelsius = Math.round(tempCelsius*10)/10;
 
-const convertToFahrenheit = function() {
+	return tempCelsius;
+}; 
+
+const convertToFahrenheit = function(tempCelsius) {
+	let tempFahrenheit = (tempCelsius*9)/5;
+	tempFahrenheit = Math.round(tempFahrenheit*10)/10;
+	tempFahrenheit += 32;
+
+	return tempFahrenheit;
 };
 
 // Do not edit below this line
