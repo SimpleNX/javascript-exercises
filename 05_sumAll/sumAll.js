09e24af247237425acf90ca6419c2e34c52b7216
SimpleNX <simplepanigrahi17@gmail.com>
2024-07-16 18:29:21 +0530

    Edit sumAll.js to add function to calculate sum

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(firstNum, secondNum) {
 
+    if(firstNum < 0 || secondNum < 0)
+        return "ERROR";
+    if(typeof(firstNum)!== "number" || typeof(secondNum)!== "number")
+        return "ERROR";
+
+    let minNum = Math.min(firstNum, secondNum);
+    let maxNum = Math.max(firstNum, secondNum);
+    let sum = 0;
+
+    for(let i = minNum; i<=maxNum; i++)
+        sum += i;
+
+    return sum;
 };
 
 // Do not edit below this line
